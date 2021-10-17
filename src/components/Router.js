1997db99ddd350185efaf0ad00b0757c7ffdabac
heeyoung <heeyoung5454@gmail.com>
2021-10-18 00:48:27 +0900

    react-router 추가 & 초기화면 설정

---
+++ src/components/Router.js
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
+import Auth from "../routes/Auth";
+import Home from "../routes/Home";
+
+const AppRouter = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  //   로그인 여부에 따라 출력되는 화면 변경
+  return (
+    <Router>
+      <Switch>
+        {isLoggedIn ? (
+          <>
+            <Route exact path='/'>
+              <Home />
+            </Route>
+          </>
+        ) : (
+          <Route exact path='/'>
+            <Auth />
+          </Route>
+        )}
+      </Switch>
+    </Router>
+  );
+};
+
+export default AppRouter;
---
+++ src/routes/EditProfile.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+//자동으로 import 시켜주기(다른 파일에서 접금할때 자동완성됨)
+const EditProfile = () => <span>EditProfile</span>;
+export default EditProfile;
