14ee3609e4ce8de634f46ed23af0b4b1ba66dc4a
heeyoung <heeyoung5454@gmail.com>
2021-10-21 00:28:53 +0900

    add LogOut

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,5 +1,19 @@
+import { authService } from "fbase";
 import React from "react";
+import { useHistory } from "react-router";
 
 //자동으로 import 시켜주기(다른 파일에서 접금할때 자동완성됨)
-const Profile = () => <span>Profile</span>;
-export default Profile;
+export default () => {
+  const history = useHistory();
+  //https://firebase.google.com/docs/reference/js/v8/firebase.auth.Auth#signout
+  const onLogOutClick = () => {
+    authService.signOut();
+    history.push("/");
+  };
+  return (
+    <>
+      Profile
+      <button onClick={onLogOutClick}>로그아웃</button>
+    </>
+  );
+};
