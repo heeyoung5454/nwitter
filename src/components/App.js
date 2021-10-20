35f5b91854bf3d9c588d7576752a8752b8254e08
heeyoung <heeyoung5454@gmail.com>
2021-10-20 23:47:50 +0900

    로그인 & 회원가입 구분

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,30 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import AppRouter from "components/Router";
 import { authService } from "fbase";
 
 function App() {
+  //firebase 초기화
+  const [init, setInit] = useState(false);
   // https://firebase.google.com/docs/reference/js/v8/firebase.auth.Auth#currentuser
   //authService.currentUser = firebase의 현재 유저정보값으로 로그인 여부 확인
-  const [isLoggedIn, setIsLoggedIn] = useState(authService.currentUser);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  //authService.onAuthStateChanged 로그인/로그아웃 시 상태변화를 알아차림(firebase 시작될때 시간이 걸리기때문에 로그인상태를 바로 인식하지 못함)
+  useEffect(() => {
+    authService.onAuthStateChanged((user) => {
+      if (user) {
+        setIsLoggedIn(true);
+      } else {
+        setIsLoggedIn(false);
+      }
+      setInit(true);
+    });
+  }, []);
+
   return (
     // 라우터에 props 잔달
     <>
-      <AppRouter isLoggedIn={isLoggedIn} />
-
+      {init ? <AppRouter isLoggedIn={isLoggedIn} /> : "로그인 중 ...."}
       <footer>&copy {new Date().getFullYear()} nwitter</footer>
     </>
   );
--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,29 +1,57 @@
 import React, { useState } from "react";
+import { authService } from "fbase";
 
 //자동으로 import 시켜주기(다른 파일에서 접금할때 자동완성됨)
 const Auth = () => {
   const [email, setEmail] = useState("");
   const [password, setPawwsord] = useState("");
+  const [newAccount, setNewAccount] = useState(true);
+  const [error, setError] = useState("");
+
   const onChange = (event) => {
     const {
       target: { name, value },
     } = event;
     if (name === "email") {
-      console.log(value);
+      setEmail(value);
     } else if (name === "password") {
       setPawwsord(value);
     }
   };
-  const onSubmit = (event) => {
+  const onSubmit = async (event) => {
     event.preventDefault();
+    try {
+      let data;
+      if (newAccount) {
+        // create account(이메일과 패스워드를 받아서 계정생성)
+        //https://firebase.google.com/docs/reference/js/v8/firebase.auth.Auth#createuserwithemailandpassword
+        data = await authService.createUserWithEmailAndPassword(email, password);
+      } else {
+        //login (파이어베이스 값 )
+        //https://firebase.google.com/docs/reference/js/v8/firebase.auth.Auth#signinwithemailandpassword
+        data = await authService.signInWithEmailAndPassword(email, password);
+      }
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+      if (error.code === "auth/weak-password") {
+        setError("패스워드 길이는 최소 6글자여야 합니다");
+        //setError(error.message);
+      } else if (error.code == "auth/email-already-in-use") {
+        setError("이미 사용중인 이메일입니다");
+      }
+    }
   };
+  const toggleAccount = () => setNewAccount((prev) => !prev);
   return (
     <div>
       <form onSubmit={onSubmit}>
         <input name='email' type='email' placeholder='Email' value={email} onChange={onChange} required />
         <input name='password' type='password' placeholder='password' value={password} onChange={onChange} required />
-        <input type='submit' value='LogIn' />
+        <input type='submit' value={newAccount ? "Create Account" : "Sign In"} />
+        {error}
       </form>
+      <span onClick={toggleAccount}> {newAccount ? "로그인하기" : "회원가입"} </span>
       <div>
         <button>Continue with Google</button>
         <button>Continue with Github</button>
